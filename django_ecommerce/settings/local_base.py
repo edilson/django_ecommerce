4aa32139890ec3afb489e7c4d67955dc335256c4
SEdilson <edilson.silva00@hotmail.com>
2019-12-06 01:46:31 -0300

    Removing local_base.py from .gitignore

---
+++ django_ecommerce/settings/local_base.py
@@ -0,0 +1,42 @@
+from .base import *
+
+DEBUG = True
+
+SECRET_KEY = 'secret'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(levelname)-8s [%(asctime)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO'
+        },
+        'celery': {
+            'handlers': ['console'],
+            'level': 'INFO'
+        }
+    }
+}
