422297fc0eb022975a9793a279b9a220850626fe
SEdilson <edilson.silva00@hotmail.com>
2019-12-11 02:14:50 -0300

    Removing django-model-utils classes

--- core/models.py
+++ core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from model_utils.fields import AutoCreatedField, AutoLastModifiedField
+# from model_utils.fields import AutoCreatedField, AutoLastModifiedField
 
 class IndexedTimeStampedModel(models.Model):
-    created = AutoCreatedField(_('criado em'), db_index=True)
-    modified = AutoLastModifiedField(_('modificado em'), db_index=True)
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
 
     class Meta:
         abstract = True
